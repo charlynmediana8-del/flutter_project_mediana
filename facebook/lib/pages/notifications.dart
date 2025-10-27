2d7a2d381464c779b8a3c66ac01d9c472a3f6668
Eida1010 <anulabac31@gmail.com>
2025-10-27 12:53:59 +0800

    midterm_mediana

--- facebook/lib/pages/notifications.dart
+++ facebook/lib/pages/notifications.dart
@@ -11,7 +11,44 @@ class _NotificationsPageState extends State<NotificationsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
-    );
+      appBar: AppBar(
+        title: const Text('Notifications'),
+        titleTextStyle: const TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+        backgroundColor: Colors.white,
+        actions: [
+          IconButton(onPressed:() {}, icon: Icon(Icons.more_horiz)),
+          IconButton(onPressed:() {}, icon: Icon(Icons.search)),
+        ],
+      ),
+      body:Column(
+        children: [
+          ListTile(
+            leading: CircleAvatar(
+              backgroundImage: AssetImage("assets/profile/prof1.jpg"),
+            ),
+            title: Text("Learned how Meta will use your info in new ways to personalize your experiences."),
+            subtitle: Text("16 hrs"),
+            trailing: Icon(Icons.thumb_up, color: Colors.blue,),
+          ),
+          ListTile(
+            leading: CircleAvatar(
+              backgroundImage: AssetImage("assets/profile/prof2.jpg"),
+            ),
+            title: Text("5 Unknown Facts recently shared 1 post."),
+            subtitle: Text("20 hrs"),
+            trailing: Icon(Icons.comment, color: Colors.green,),
+          ),
+          ListTile(
+            leading: CircleAvatar(
+              backgroundImage: AssetImage("assets/profile/prof3.jpg"),
+            ),
+            title: Text("Fandango posted a new reel: Diego Luna, Jennifer Lopez, and Tonatiuh star in KissOf TheSpiderWoman, in theater..."),
+            subtitle: Text("2 hrs"),
+            trailing: Icon(Icons.person_add, color: Colors.orange,),
+          ),
+          const Padding(padding:  EdgeInsets.all(8.0)),
+          child: Text("Today", style: TextStyle(fontWeight: FontWeight.bold),),
+        ]),
+  )
+    ;}
   }
-}
